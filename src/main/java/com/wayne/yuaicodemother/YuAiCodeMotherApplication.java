e639f9b94c34cfbce981f9649519d793cb4ef099
Wayne <1178690569@qq.com>
2025-10-15 21:40:00 +0800

    初始化

---
+++ src/main/java/com/wayne/yuaicodemother/YuAiCodeMotherApplication.java
@@ -0,0 +1,13 @@
+package com.wayne.yuaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuAiCodeMotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuAiCodeMotherApplication.class, args);
+    }
+
+}
