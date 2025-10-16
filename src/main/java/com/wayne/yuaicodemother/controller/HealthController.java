f8e92a9265b85e42979576830379e6d893ece7f5
Wayne <1178690569@qq.com>
2025-10-16 20:22:46 +0800

    第二期 后端初始化

---
+++ src/main/java/com/wayne/yuaicodemother/controller/HealthController.java
@@ -0,0 +1,18 @@
+package com.wayne.yuaicodemother.controller;
+
+import com.wayne.yuaicodemother.common.BaseResponse;
+import com.wayne.yuaicodemother.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success( "ok");
+    }
+}
+
