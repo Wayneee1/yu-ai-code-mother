379201ab8899cfa977d3d034adc636ed2b91455f
Wayne <1178690569@qq.com>
2025-10-19 20:13:24 +0800

    第二期 后端用户模块开发 用户注册、用户登录、用户注销等 全局权限校验 AOP 增删改查 JSON丢失精度

--- src/main/java/com/wayne/yuaicodemother/YuAiCodeMotherApplication.java
+++ src/main/java/com/wayne/yuaicodemother/YuAiCodeMotherApplication.java
@@ -1,9 +1,11 @@
 package com.wayne.yuaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wayne.yuaicodemother.mapper")
 public class YuAiCodeMotherApplication {
 
     public static void main(String[] args) {
