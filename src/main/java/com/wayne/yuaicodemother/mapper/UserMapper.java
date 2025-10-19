db61350e33e652bdc591cf717e388b73009861f1
Wayne <1178690569@qq.com>
2025-10-19 20:14:27 +0800

    第二期 后端用户模块开发 用户注册、用户登录、用户注销等 全局权限校验 AOP 增删改查 JSON丢失精度

---
+++ src/main/java/com/wayne/yuaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.wayne.yuaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.wayne.yuaicodemother.model.entity.User;
+
+/**
+ *  映射层。
+ *
+ * @author <a href="https://gitee.com/WayneZhou0828">Wayne Zhou</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/wayne/yuaicodemother/service/impl/UserServiceImpl.java
@@ -0,0 +1,187 @@
+package com.wayne.yuaicodemother.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.spring.service.impl.ServiceImpl;
+import com.wayne.yuaicodemother.exception.BusinessException;
+import com.wayne.yuaicodemother.exception.ErrorCode;
+import com.wayne.yuaicodemother.model.dto.UserQueryRequest;
+import com.wayne.yuaicodemother.model.entity.User;
+import com.wayne.yuaicodemother.mapper.UserMapper;
+import com.wayne.yuaicodemother.model.enums.UserRoleEnum;
+import com.wayne.yuaicodemother.model.vo.LoginUserVO;
+import com.wayne.yuaicodemother.model.vo.UserVO;
+import com.wayne.yuaicodemother.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.wayne.yuaicodemother.constant.UserConstant.USER_LOGIN_STATE;
+
+/**
+ *  服务层实现。
+ *
+ * @author <a href="https://gitee.com/WayneZhou0828">Wayne Zhou</a>
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User>  implements UserService{
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        // 1. 校验
+        if (StrUtil.hasBlank(userAccount, userPassword, checkPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
+        }
+        if (userAccount.length() < 4) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户账号过短");
+        }
+        if (userPassword.length() < 8 || checkPassword.length() < 8) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户密码过短");
+        }
+        if (!userPassword.equals(checkPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "两次输入的密码不一致");
+        }
+        // 2. 检查是否重复
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("userAccount", userAccount);
+        long count = this.mapper.selectCountByQuery(queryWrapper);
+        if (count > 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号重复");
+        }
+        // 3. 加密
+        String encryptPassword = getEncryptPassword(userPassword);
+        // 4. 插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        user.setUserName("无名");
+        user.setUserRole(UserRoleEnum.USER.getValue());
+        boolean saveResult = this.save(user);
+        if (!saveResult) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "注册失败，数据库错误");
+        }
+        return user.getId();
+    }
+
+    @Override
+    public LoginUserVO getLoginUserVO(User user) {
+        if(user == null){
+            return null;
+        }
+        LoginUserVO loginUserVO = new LoginUserVO();
+        BeanUtil.copyProperties(user, loginUserVO);
+        return loginUserVO;
+    }
+
+    @Override
+    public LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        // 1. 校验参数
+        if (StrUtil.hasBlank(userAccount, userPassword)) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "参数为空");
+        }
+        if (userAccount.length() < 4) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "账号过短");
+        }
+        if (userPassword.length() < 8) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "密码过短");
+        }
+
+        // 2. 加密
+        String encryptPassword = getEncryptPassword(userPassword);
+        // 3. 查询用户是否存在
+        QueryWrapper queryWrapper = new QueryWrapper();
+        queryWrapper.eq("userAccount", userAccount);
+        queryWrapper.eq("userPassword", encryptPassword);
+        User user = this.mapper.selectOneByQuery(queryWrapper);
+        if (user == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "用户不存在或密码错误");
+        }
+        // 4. 如果用户存在，记录用户的登录态
+        request.getSession().setAttribute(USER_LOGIN_STATE, user);
+        // 5. 返回脱敏用户信息
+        return this.getLoginUserVO(user);
+    }
+
+    @Override
+    public User getLoginUser(HttpServletRequest request) {
+        // 判断用户是否登录
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        User currentUser = (User) userObj;
+        if (currentUser == null || currentUser.getId() == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        // 从数据库查询,数据库信息是最新的（追求性能的话可以注释，直接走缓存）
+        long userId = currentUser.getId();
+        //currentUser = this.mapper.selectOneById(userId);
+        currentUser = this.getById(userId);
+        if (currentUser == null) {
+            throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+        }
+        return currentUser;
+    }
+
+    @Override
+    public UserVO getUserVO(User user) {
+        if (user == null) {
+            return null;
+        }
+        UserVO userVO = new UserVO();
+        BeanUtil.copyProperties(user, userVO);
+        return userVO;
+    }
+
+    @Override
+    public List<UserVO> getUserVOList(List<User> userList) {
+        if (CollUtil.isEmpty(userList)) {
+            return new ArrayList<>();
+        }
+        return userList.stream().map(this::getUserVO).collect(Collectors.toList());
+    }
+
+    @Override
+    public QueryWrapper getQueryWrapper(UserQueryRequest userQueryRequest) {
+        if (userQueryRequest == null) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR, "请求参数为空");
+        }
+        Long id = userQueryRequest.getId();
+        String userAccount = userQueryRequest.getUserAccount();
+        String userName = userQueryRequest.getUserName();
+        String userProfile = userQueryRequest.getUserProfile();
+        String userRole = userQueryRequest.getUserRole();
+        String sortField = userQueryRequest.getSortField();
+        String sortOrder = userQueryRequest.getSortOrder();
+        return QueryWrapper.create()
+                .eq("id", id)
+                .eq("userRole", userRole)
+                .like("userAccount", userAccount)
+                .like("userName", userName)
+                .like("userProfile", userProfile)
+                .orderBy(sortField, "ascend".equals(sortOrder));
+    }
+
+
+    @Override
+    public boolean userLogout(HttpServletRequest request) {
+        // 判断用户是否登录
+        Object userObj = request.getSession().getAttribute(USER_LOGIN_STATE);
+        if (userObj == null) {
+            throw new BusinessException(ErrorCode.OPERATION_ERROR,"用户未登录");
+        }
+        // 移除登录态
+        request.getSession().removeAttribute(USER_LOGIN_STATE);
+        return true;
+    }
+
+    @Override
+    public String getEncryptPassword(String userPassword) {
+        // 盐值，混淆密码
+        final String SALT = "yupi";
+        return DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+    }
+
+}
