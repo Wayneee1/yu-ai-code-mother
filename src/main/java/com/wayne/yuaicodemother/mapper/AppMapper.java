d985445339784f780bd1ae1836894b0f7893f3a0
Wayne <1178690569@qq.com>
2025-11-20 21:10:58 +0800

    第五期 完成基本功能

---
+++ src/main/java/com/wayne/yuaicodemother/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.wayne.yuaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.wayne.yuaicodemother.model.entity.App;
+
+/**
+ *  映射层。
+ *
+ * @author <a href="https://github.com/liyupi">Wayne Zhou</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
---
+++ src/main/java/com/wayne/yuaicodemother/service/AppService.java
@@ -0,0 +1,32 @@
+package com.wayne.yuaicodemother.service;
+
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.wayne.yuaicodemother.model.dto.app.AppQueryRequest;
+import com.wayne.yuaicodemother.model.entity.App;
+import com.wayne.yuaicodemother.model.vo.AppVO;
+
+import java.util.List;
+
+/**
+ *  服务层。
+ *
+ * @author <a href="https://github.com/liyupi">Wayne Zhou</a>
+ */
+public interface AppService extends IService<App> {
+    /**
+     * 获取应用封装类
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+}
